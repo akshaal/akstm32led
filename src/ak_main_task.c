3b54cfcdbdd797049344bd87cfeca69b58aa95f2
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-03-08 00:02:21 +0100

    b

--- src/ak_main_task.c
+++ src/ak_main_task.c
@@ -10,18 +10,13 @@ void ak_create_main_task() {
 
 static void ak_main_task(void *argument) {
     for(;;) {
-        for (int i = 0; i < 100; i++) {
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-            ak_task_delay(20);
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-            ak_task_delay(20);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-            ak_task_delay(80);
-            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-            ak_task_delay(80);
+        for (int i = 0; i < 100; i += 20) {
+            for (int z = 0; z < 100 / i; i++) {
+                HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+                ak_task_delay(i);
+                HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+                ak_task_delay(i);
+            }
         }
     }
 }
