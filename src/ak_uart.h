233ba8e06c1219cb2204e45c58b738a728eefffd
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-03-11 12:27:25 +0100

    blah

--- src/ak_uart.h
+++ src/ak_uart.h
@@ -3,8 +3,8 @@
 #ifndef __ak_uart_h
 #define __ak_uart_h
 
-#define AK_UART_TX_QUEUE_SIZE 50
-#define AK_UART_RX_QUEUE_SIZE 10
+#define AK_UART_TX_QUEUE_SIZE 10
+#define AK_UART_RX_QUEUE_SIZE 4
 #define AK_UART_RX_CHAR_QUEUE_SIZE 100
 #define AK_UART_RX_BUF_LEN 80
 
